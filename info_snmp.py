ec7b7fdea46e3d8211507fae018d103997e09d90
Breizhhhh <101636738+Breizhhhh@users.noreply.github.com>
2024-02-02 02:50:13 +0100

    affichage ssh

--- info_snmp.py
+++ info_snmp.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 from pysnmp.hlapi import *
+import paramiko
 
 app = Flask(__name__)
 
 # Configuration de l'agent SNMP
 snmp_port = 161
 snmp_community = 'public'
+
+# Configuration SSH
+ssh_username = 'user'
+ssh_password = 'bonjour'
+ssh_port = 22
 
 # Fonction pour effectuer la correspondance entre les OID et leurs significations
 def map_oid_to_name(oid):
@@ -47,12 +53,34 @@ def snmp_get(oids, target_ip):
 
     return results
 
-# Route to serve the HTML page
+def ssh_execute_command(target_ip, command):
+    # Create an SSH client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        # Connect to the SSH server
+        ssh.connect(target_ip, port=ssh_port, username=ssh_username, password=ssh_password)
+
+        # Execute the command
+        stdin, stdout, stderr = ssh.exec_command(command)
+
+        # Get the command output
+        output = stdout.read().decode('utf-8')
+
+        return output
+
+    finally:
+        # Close the SSH connection
+        ssh.close()
+
+# Route pour servir la page HTML
 @app.route('/')
 def index():
-    return render_template('index.html')
+    additional_text = "Ceci est un texte supplémentaire à afficher dans la page."
+    return render_template('page01.html', additional_text=additional_text)
 
-# Route to handle SNMP scanning
+# Route pour gérer le balayage SNMP
 @app.route('/scan', methods=['POST'])
 def scan():
     target_ip = request.json.get('ipAddress')
@@ -66,6 +94,15 @@ def scan():
     print(results)
 
     return jsonify({target_ip: results})
+
+# Route pour gérer la commande SSH
+@app.route('/ssh', methods=['POST'])
+def execute_ssh_command():
+    target_ip = request.json.get('ipAddress')
+    ssh_command_output = ssh_execute_command(target_ip, 'ls')  # Exemple avec la commande 'ls'
+    print(ssh_command_output)
+
+    return jsonify({'ssh_command_output': ssh_command_output})
 
 if __name__ == '__main__':
     app.run(debug=True)
